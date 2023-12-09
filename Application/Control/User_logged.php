5ffdc42c0cba3c8cfdd4486452c05a2e0760049f
thiagoap <thiago_ap_@hotmail.com>
2023-12-09 04:51:37 -0300

    ajuste na validação do login

--- Application/Control/User_logged.php
+++ Application/Control/User_logged.php
@@ -33,52 +33,56 @@
 
                 if(empty($user_dataset)) {
 
+                    Transaction::close(); 
+
                     header("Location:index.php?class=Login&method=unregistered_User");
 
-                }
+                }else {
 
 
-                $object = $user_dataset[0];
+                    $object = $user_dataset[0];
 
-                
-                $_SESSION['id'] = $object->id;
-
-                // customer será usado na API do gateway de pagamento da empresa PAGARME
-
-                $telefone = '+55' . $object->ddd_telefone . $object->telefone;
-                
-                // coverte o valor para string
-
-                $telefone = strval($telefone);
-
-                $_SESSION['customer'] = [
                     
-                    'external_id' => $object->id,
-                    'name' => $object->nome,
-                    'type' => 'individual',
-                    'country' => 'br',
+                    $_SESSION['id'] = $object->id;
 
-                    'documents' => [
+                    // customer será usado na API do gateway de pagamento da empresa PAGARME
 
-                        [
-                            'type' => 'cpf',
-                            'number' => $object->cpf
-                        ]
+                    $telefone = '+55' . $object->ddd_telefone . $object->telefone;
+                    
+                    // coverte o valor para string
 
-                    ],
+                    $telefone = strval($telefone);
 
-                    'phone_numbers' => [$telefone],
-                    'email' => $object->email
-                ];                   
-                
+                    $_SESSION['customer'] = [
+                        
+                        'external_id' => $object->id,
+                        'name' => $object->nome,
+                        'type' => 'individual',
+                        'country' => 'br',
 
-                $authentication = new Authentication();
-                $authentication->set_Authentication($object->email);
-                $auth = $authentication->get_Authentication();
-                
-                Transaction::close(); 
+                        'documents' => [
 
-                header("Location: index.php?class=Home&authentication=$auth");
+                            [
+                                'type' => 'cpf',
+                                'number' => $object->cpf
+                            ]
+
+                        ],
+
+                        'phone_numbers' => [$telefone],
+                        'email' => $object->email
+                    ];                   
+                    
+
+                    $authentication = new Authentication();
+                    $authentication->set_Authentication($object->email);
+                    $auth = $authentication->get_Authentication();
+                    
+                    Transaction::close(); 
+
+                    header("Location: index.php?class=Home&authentication=$auth");
+
+                }
 
             }else {
 
